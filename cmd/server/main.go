c7bd702165a7533b37ea1afebf004dee71ef0c06
vovainside <vovainside@gmail.com>
2020-05-15 14:35:36 +0600

    add tls for prod env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
 	"github.com/refto/server/config"
 	"github.com/refto/server/logger"
@@ -16,28 +17,42 @@ func main() {
 	conf := config.Get()
 	logger.Setup()
 
+	log.Println("refto." + conf.AppEnv + " serving at " + conf.Server.Host + ":" + conf.Server.Port)
+
+	if config.IsReleaseEnv() {
+		gin.SetMode(conf.AppEnv)
+	}
+
 	r := gin.Default()
 	route.Register(r)
 
-	server := &http.Server{
-		Addr:    conf.Server.Host + ":" + conf.Server.Port,
-		Handler: r,
-	}
+	if !config.IsReleaseEnv() {
+		server := &http.Server{
+			Addr:    conf.Server.Host + ":" + conf.Server.Port,
+			Handler: r,
+		}
 
-	quit := make(chan os.Signal)
-	signal.Notify(quit, os.Interrupt)
+		quit := make(chan os.Signal)
+		signal.Notify(quit, os.Interrupt)
 
-	go func() {
-		<-quit
-		if err := server.Close(); err != nil {
+		go func() {
+			<-quit
+			if err := server.Close(); err != nil {
+				log.Fatal(err.Error())
+			}
+		}()
+
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
 			log.Fatal(err.Error())
 		}
-	}()
-
-	log.Println("refto." + conf.AppEnv + " serving at " + conf.Server.Host + ":" + conf.Server.Port)
-	err := server.ListenAndServe()
-	if err != nil && err != http.ErrServerClosed {
-		log.Fatal(err.Error())
+	} else {
+		//On Linux, you can use setcap to grant your binary the permission to bind low ports:
+		//$ sudo setcap cap_net_bind_service=+ep /path/to/your/binary
+		err := autotls.Run(r, conf.Server.Host)
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal(err.Error())
+		}
 	}
 
 	log.Println("refto." + conf.AppEnv + " server closed")
--- go.mod
+++ go.mod
@@ -5,12 +5,13 @@ go 1.14
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/brianvoe/gofakeit v3.18.0+incompatible
-	github.com/davecgh/go-spew v1.1.1
 	github.com/ghodss/yaml v1.0.0
 	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/autotls v0.0.0-20200314141124-cc69476aef2a
 	github.com/gin-gonic/gin v1.6.2
 	github.com/go-pg/migrations/v7 v7.1.10
 	github.com/go-pg/pg/v9 v9.1.6
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/lib/pq v1.4.0
 	github.com/sirupsen/logrus v1.5.0
 	github.com/stretchr/testify v1.5.1
